99ce9087e821c2d0b5a9d852a6d7cee057649f9f
Montana Knight <montknight@gmail.com>
2022-10-14 16:17:42 -0400

    Added description to binning_numbers function

--- R/binning_numbers.R
+++ R/binning_numbers.R
@@ -1,3 +1,16 @@
+#' Binning numbers function
+#'
+#' @param sequence_to_cut A sequence of numbers that you want to know how many numbers are in however many bins
+#' @param desired_bin_size The bin size you want
+#' @param right default is FALSE
+#'
+#' @return a frequency table
+#' @export
+#'
+#' @examples
+#' sequence_to_cut <- 0:100
+#' binning_numbers(sequence_to_cut, 5)
+
 
 binning_numbers <- function(sequence_to_cut, desired_bin_size, right = FALSE){
   breaks <- seq(min(sequence_to_cut),max(sequence_to_cut),by=desired_bin_size)
